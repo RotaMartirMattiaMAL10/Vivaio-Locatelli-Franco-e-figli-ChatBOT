b2385ab0fc8d67f7bde6941c19234e627cd080e6
RotaMartirMattiaMAL10 <monicafrancoefigli@gmail.com>
2026-03-10 11:42:26 +0100

    Create app/core/config.py with configuration settings

---
+++ app/core/config.py
@@ -0,0 +1,8 @@
+# Configuration settings
+
+class Config:
+    DEBUG = True
+    DATABASE_URI = 'sqlite:///app.db'
+    SECRET_KEY = 'your_secret_key_here'
+    ALLOWED_HOSTS = ['localhost']
+    PORT = 5000
